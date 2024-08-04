2bd12be644b6fa8a4bb55d0d273e9f80231a6ad2
Adam Youlin He <ayh8@cornell.edu>
2024-08-03 23:22:13 -0400

    Update predict_clipnet_test.py

--- predict_clipnet_test.py
+++ predict_clipnet_test.py
@@ -5,7 +5,7 @@ import pandas as pd
 from scipy.spatial.distance import jensenshannon as jsd
 from scipy.stats import pearsonr
 
-os.environ["CUDA_VISIBLE_DEVICES"] = str(1)
+os.environ["CUDA_VISIBLE_DEVICES"] = 1
 from puffin import Puffin, puffin_predict
 
 puffin_model = Puffin(use_cuda=True)
